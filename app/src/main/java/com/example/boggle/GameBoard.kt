c03dd730d1401f9f57a2946ea26d3f9587c259e4
picoting <lindsayl.6857@gmail.com>
2024-04-01 15:24:16 -0400

    creating gameboard grid+logic

--- app/src/main/java/com/example/boggle/GameBoard.kt
+++ app/src/main/java/com/example/boggle/GameBoard.kt
@@ -4,12 +4,42 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.GridLayout
 import androidx.fragment.app.Fragment
 
 class GameBoard: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.gameboard, container, false)
+
+        populateGrid()
+    }
+
+    private fun populateGrid() {
+        val letterGrid: GridLayout = view?.findViewById(R.id.letterGrid) ?: return
+        letterGrid.removeAllViews() // clear the grid
+
+        val letters = generateRandomLetters()
+
+        for (i in letters.indices) {
+            val button = Button(context).apply {
+                text = letters[i].toString()
+                layoutParams = GridLayout.LayoutParams().apply {
+                    //gahhhhhhh
+                }
+                setOnClickListener {
+                    //handle gameplay
+                }
+            }
+            letterGrid.addView(button)
+        }
+    }
+
+    private fun generateRandomLetters(): List<Char> {
+        val vowels = listOf('A', 'E', 'I', 'O', 'U')
+        val consonants = ('A'..'Z').filterNot { it in vowels }
+        return vowels
     }
 }
